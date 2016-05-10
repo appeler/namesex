e1bbcd86a52850d9ce4f3f00a0197b6a70a6279b
Gaurav Sood <gsood07@gmail.com>
2016-05-10 08:28:03 -0400

    declare dependency and add basic namesex pkg file

---
+++ R/namesex.R
@@ -0,0 +1,17 @@
+#' @title namesex: Predict sex of person based on first name
+#' 
+#' @name namesex-package
+#' @aliases namesex
+#'
+#' @description Predict sex of the person based on the first name. The package currently
+#' supports Indian and Brazilian names. Package supports three ways of learning the sex of the 
+#' person: raw data of brazilian and indian politicians, and one ML model based off 
+#' the name data.
+#'
+#' To learn how to use namesex, see this vignette: \url{vignettes/namesex.html}. 
+#' 
+#' @importFrom utils read.table
+#' @importFrom namesexdata brazilNames
+#' @docType package
+#' @author Gaurav Sood and Luke Sonnet
+NULL
